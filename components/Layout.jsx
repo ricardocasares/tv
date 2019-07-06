883ad2c8b30017dbdb9b5dd7502922d96a2269bf
Ricardo Casares <ricardocasares@gmail.com>
2019-07-06 02:59:39 +0200

    add image

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,10 @@
 import styled from "@emotion/styled";
+import rainbow from "../assets/tv.gif";
 
 export const Layout = styled.div`
   display: flex;
   flex-direction: column;
-  background-image: url(/img/tv.gif);
+  background-image: url(${rainbow});
   background-size: cover;
   height: 100%;
   overflow-y: scroll;
